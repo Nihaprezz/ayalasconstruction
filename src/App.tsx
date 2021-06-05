7a2da5f56b9752db556b1050ff5c37ceda5bf8eb
Nihaprezz <sergio13prez@outlook.com>
2021-06-04 23:23:16 -0400

    Initial header component created

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {
   Route,
 } from "react-router-dom";
 import {Header} from "./components/Header";
+import "./App.scss";
 
 const App: React.FunctionComponent = () => {
   return (
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../images/logo.svg";
+import "../styles/header.scss";
 
 export const Header: React.FunctionComponent = () => {
+    const [openNavBar, setOpenNavBar] = React.useState<boolean>(false);
+    
     return (
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
+        <nav className="navbar is-black" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <a id="navbar-img-logo" 
+                    className="navbar-item" 
+                    href="/">
+                    <img src={Logo} width="112" height="28" alt="logo-img"/>
+                </a>
+                <div
+                    role="button" 
+                    className={`navbar-burger ${openNavBar ? "is-active": ""}`} 
+                    aria-label="menu" 
+                    aria-expanded="false" 
+                    data-target="navbarMain"
+                    onClick={() => setOpenNavBar(!openNavBar)}
+                >
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </div>
+            </div>
+            
+            <div 
+                id="navbarMain" 
+                className={`navbar-menu ${openNavBar ? "is-active" : ""}`}
+            >
+                <div className="navbar-end">
+                    <Link to="/" className="navbar-item">
+                        Home
+                    </Link>
+
+                    <Link to="/services" className="navbar-item">
+                        Services
+                    </Link>
+
+                    <Link to="/portfolio" className="navbar-item">
+                        Portfolio
+                    </Link>
+
+                    <Link to="/contact" className="navbar-item">
+                        Contact Us
+                    </Link>
+                </div>
+            </div>
         </nav>
       </div>
     );
