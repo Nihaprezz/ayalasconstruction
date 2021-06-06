a5c0a946320175bdeb61bf52534b90597991f16c
Nihaprezz <sergio13prez@outlook.com>
2021-06-06 14:38:56 -0400

    gitignore added, initial landing page and some minor style updates

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "../styles/home.scss";
+// import Landing from "../images/landing.svg";
+// import Landing2 from "../images/landing-design.png";
 
 export const Home: React.FunctionComponent = () => {
     return (
         <div className="main-home-page">
-            <h1>Home Page</h1>
+            <div className="home-page-content">
+                <h1 className="title">Ayala's Construction LLC.</h1>
+                <p className="subtitle">Landscaping, Erosion Control, and Site Work</p>
+                <button className="button is-primary">Contact Us <span>571-464-3735</span></button>
+            </div>
         </div>
     )
 };
